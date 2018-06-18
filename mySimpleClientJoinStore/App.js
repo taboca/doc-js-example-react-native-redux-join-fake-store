08ff65acd6343e511a938fa298489d5a11cfea99
Marcio Galli <taboca@gmail.com>
2018-06-18 19:28:18 -0300

    Simply loading the whole list

--- mySimpleClientJoinStore/App.js
+++ mySimpleClientJoinStore/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import store from './app/configStore'
+import { Provider } from 'react-redux'
+import Home  from './app/components/Home'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Provider store={store}>
+          <Home />
+        </Provider>
       </View>
     );
   }
---
+++ mySimpleClientJoinStore/app/containers/LoadButton.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux'
+import { get_data_products } from '../actions'
+import LoadButton from '../components/LoadButton'
+
+const mapStateToProps = state => ({
+})
+
+const mapDispatchToProps = dispatch => ({
+  get_data_products: () => dispatch(get_data_products())
+})
+
+//export default connect(mapStateToProps, mapDispatchToProps)(AddList_button);
+export default connect(mapStateToProps, mapDispatchToProps)(LoadButton);
